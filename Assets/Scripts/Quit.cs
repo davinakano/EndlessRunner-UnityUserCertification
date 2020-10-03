dbcc78e6b09c36bd65350634106883215de9b9ff
hgholami <35938542+hgholami@users.noreply.github.com>
2020-10-03 01:05:55 -0700

    Added quit feature. Pressing Escape key will quit game to desktop.

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+}
